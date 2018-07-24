86f000118a7df4c17e5de9f1f49d3306723a1706
chengjo0 <john.cheng@futurice.com>
2018-07-24 13:14:19 +0300

    Uses refactored validation function

--- src/containers/RegisterUserInformationScreen/RegisteruserInformationScreen.js
+++ src/containers/RegisterUserInformationScreen/RegisteruserInformationScreen.js
@@ -1,96 +1,26 @@
 import React from 'react';
-import {reduxForm, SubmissionError} from "redux-form";
-import {connect} from "react-redux";
-import {NavigationActions} from "react-navigation";
-import UserInformationForm from "../../components/UserInformationForm";
-import moment from 'moment';
-import {validateEmail, validateUsername} from "../../actions/register";
+import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
+import UserInformationForm from '../../components/UserInformationForm';
+import { validateUserInformation } from '../../utils/reduxValidations';
 
 const mapDispatchToProps = dispatch => ({
-  goToPersonalitiesScreen: () => dispatch(NavigationActions.navigate({routeName: 'Personalities'}))
+  goToPersonalitiesScreen: () =>
+    dispatch(NavigationActions.navigate({ routeName: 'Personalities' })),
 });
 
 const UserInformationScreen = props => (
-  <UserInformationForm handleSubmit={(data) => props.handleSubmit(data)} {...props}/>
+  <UserInformationForm
+    handleSubmit={data => props.handleSubmit(data)}
+    {...props}
+  />
 );
 
-async function validateUserInformation(values, dispatch) {
-  let err = null;
-
-  if (!values.username) {
-    err = {
-      ...err,
-      username: 'Enter a valid username',
-    };
-  } else {
-    const nbOfUsersWithUsername = await dispatch(validateUsername(values.username));
-    if (nbOfUsersWithUsername > 0) {
-      err = {
-        ...err,
-        username: `That username is already taken : ${values.username}`,
-      };
-    }
-  }
-
-  function emailNotValid(email) {
-    const emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    return !emailRegEx.test(email);
-  }
-
-  if (!values.email || emailNotValid(values.email)) {
-    err = {
-      ...err,
-      email: 'Enter a valid email (ex. foo@bar.com)',
-    };
-  } else {
-    const nbOfUsersWithEmail = await dispatch(validateEmail(values.email));
-    if (nbOfUsersWithEmail.length > 0) {
-      err = {
-        ...err,
-        email: `This email is already used : ${values.email}`,
-      };
-    }
-  }
-
-  if (!values.password) {
-    err = {
-      ...err,
-      password: 'Enter a valid password',
-    };
-  }
-
-  function checkAge(birthDate) {
-    return moment().year() - birthDate >= 16;
-  }
-
-  if (!values.birthyear) {
-    err = {
-      ...err,
-      birthyear: 'Enter a valid birth year',
-    };
-  } else if (!checkAge(values.birthyear)) {
-    err = {
-      ...err,
-      birthyear: 'User should be at least 16',
-    };
-  }
-
-  if (!values.genders || values.genders.length <= 0) {
-    err = {
-      ...err,
-      genders: 'Select at least a gender',
-    };
-  }
-
-  if (err) {
-    throw new SubmissionError({
-      ...err,
-      _error: 'Login failed !',
-    });
-  }
-}
-
-export default connect(null, mapDispatchToProps)(
+export default connect(
+  null,
+  mapDispatchToProps,
+)(
   reduxForm({
     form: 'register',
     destroyOnUnmount: false,
@@ -98,6 +28,6 @@ export default connect(null, mapDispatchToProps)(
     onSubmit: (value, dispatch) => validateUserInformation(value, dispatch),
     onSubmitSuccess: (result, dispatch, props) => {
       props.goToPersonalitiesScreen();
-    }
-  })(UserInformationScreen)
+    },
+  })(UserInformationScreen),
 );
