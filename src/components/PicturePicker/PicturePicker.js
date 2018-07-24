e14765e212425e338a77206b5bb4004b23bad653
chengjo0 <john.cheng@futurice.com>
2018-07-24 13:13:08 +0300

    Now uses uri to show image

--- src/components/PicturePicker/PicturePicker.js
+++ src/components/PicturePicker/PicturePicker.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ImagePicker, Permissions} from 'expo';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import { ImagePicker, Permissions } from 'expo';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 export default class PhotoBox extends React.Component {
   state = {
@@ -22,22 +22,27 @@ export default class PhotoBox extends React.Component {
 
     if (!result.cancelled) {
       input.onChange(result);
-      this.setState({selected: true, picture: result});
+      this.setState({ selected: true, picture: result });
     }
   };
 
   renderPicture() {
     if (this.state.picture) {
       return (
-        <Image style={{width: 93, height: 93}} source={this.state.picture}/>
+        <Image
+          style={{ width: 93, height: 93 }}
+          source={{ uri: this.state.picture.uri }}
+        />
       );
     }
     return (
-      <Text style={{
-        fontSize: 50,
-        color: '#60686d',
-        textAlign: 'center',
-      }}>
+      <Text
+        style={{
+          fontSize: 50,
+          color: '#60686d',
+          textAlign: 'center',
+        }}
+      >
         +
       </Text>
     );
@@ -45,11 +50,11 @@ export default class PhotoBox extends React.Component {
 
   deletePicture(input) {
     input.onChange({});
-    this.setState({picture: ''});
+    this.setState({ picture: '' });
   }
 
   render() {
-    const {input} = this.props;
+    const { input } = this.props;
 
     return (
       <View>
@@ -68,19 +73,22 @@ export default class PhotoBox extends React.Component {
             }}
             onPress={() => this.deletePicture(input)}
           >
-            <Text style={{color: 'white', fontWeight: 'bold'}}>X</Text>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>X</Text>
           </TouchableOpacity>
         ) : null}
-        <TouchableOpacity style={{
-          width: 93,
-          height: 93,
-          backgroundColor: '#e8e9e8',
-          marginRight: 15,
-          marginTop: 15,
-          borderWidth: 1,
-          borderColor: '#839297',
-          justifyContent: 'center',
-        }} onPress={() => this.openImageGallery(input)}>
+        <TouchableOpacity
+          style={{
+            width: 93,
+            height: 93,
+            backgroundColor: '#e8e9e8',
+            marginRight: 15,
+            marginTop: 15,
+            borderWidth: 1,
+            borderColor: '#839297',
+            justifyContent: 'center',
+          }}
+          onPress={() => this.openImageGallery(input)}
+        >
           {this.renderPicture()}
         </TouchableOpacity>
       </View>
