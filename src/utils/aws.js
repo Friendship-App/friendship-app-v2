9a78d6142ad9e9f76330faf39d7bf13a67bfc240
Daniel Landau <daniel@landau.fi>
2018-12-30 13:03:46 +0200

    Only return from aws when image has been actually uploaded

--- src/utils/aws.js
+++ src/utils/aws.js
@@ -17,30 +17,33 @@ export const getPreSignedUrl = async (type, formValues) => {
   const fragments = data.url.split('/');
   const fileName = `${data.itemName}-${fragments[fragments.length - 1]}`;
 
-  return await fetch(`${apiRoot}${link}${fileName}&file-type=${data.imgType}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      const { signedRequest, url } = myJson;
+  const response = await fetch(
+    `${apiRoot}${link}${fileName}&file-type=${data.imgType}`,
+  );
+  const myJson = await response.json();
+  const { signedRequest, url } = myJson;
 
-      const xhr = new XMLHttpRequest();
-      xhr.open('PUT', signedRequest);
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            console.log('Image successfully uploaded to S3');
-          } else {
-            console.log('Error while sending the image to S3');
-          }
+  const xhrPromise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('Image successfully uploaded to S3');
+          resolve(url);
+        } else {
+          console.log('Error while sending the image to S3');
+          resolve(undefined);
         }
-      };
-      xhr.setRequestHeader('Content-Type', 'image/jpeg');
-      xhr.send({
-        uri: data.url,
-        type: 'image/jpeg',
-        name: fileName,
-      });
-      return url;
+      }
+    };
+    xhr.setRequestHeader('Content-Type', 'image/jpeg');
+    xhr.send({
+      uri: data.url,
+      type: 'image/jpeg',
+      name: fileName,
     });
+  });
+  const xhrResult = await xhrPromise;
+  return xhrResult;
 };
