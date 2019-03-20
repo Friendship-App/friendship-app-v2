1f6ad3c53f3478ff7a294f473515a335080ffb8b
Petri Huovinen <petri.huovinen@houston-inc.com>
2019-03-20 13:13:18 +0200

    Rename events to activities

--- src/routes/EventsStack/EventsStack.js
+++ src/routes/EventsStack/EventsStack.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {createStackNavigator} from 'react-navigation';
-import EventsScreen from "../../containers/EventsScreen";
-import {fonts} from "../../styles";
+import { createStackNavigator } from 'react-navigation';
+import EventsScreen from '../../containers/EventsScreen';
+import { fonts } from '../../styles';
 
 const EventsStack = createStackNavigator({
   Events: {
     screen: EventsScreen,
     navigationOptions: {
-      title: 'Events',
+      title: 'Activities',
       headerTitleStyle: {
-        fontFamily: fonts.SEMI_BOLD
-      }
-    }
-  }
+        fontFamily: fonts.SEMI_BOLD,
+      },
+    },
+  },
 });
 
 export default EventsStack;
