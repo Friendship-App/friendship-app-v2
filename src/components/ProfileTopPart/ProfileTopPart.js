860fc8a276c7c0786faa26cc68b6a20116a6d6f3
Petri Huovinen <petri.huovinen@houston-inc.com>
2019-04-16 20:53:29 +0300

    Show age in same way in persons list and in profile page

--- src/components/ProfileTopPart/ProfileTopPart.js
+++ src/components/ProfileTopPart/ProfileTopPart.js
@@ -12,6 +12,7 @@ import {
 import Icon from 'react-native-vector-icons/Ionicons';
 import waveShape from '../../../assets/img/curve/curve.png';
 import { colors, fonts, fontSizes, paddings } from '../../styles';
+import { getFormattedAge } from '../../utils/ageFormatter';
 
 const ProfileTopPart = props => {
   const {
@@ -26,30 +27,6 @@ const ProfileTopPart = props => {
     genderList,
     commonTagPercent,
   } = props;
-
-  const getAge = () => {
-    const parsedBirthYear = parseInt(birthyear);
-    const now = new Date();
-    let age = now.getFullYear() - parsedBirthYear;
-
-    const early = [0, 1, 2, 3];
-    const mid = [4, 5, 6];
-    const late = [7, 8, 9];
-    let ageName = '';
-    const lastDigit = age.toString().substr(age.toString().length - 1);
-    if (age < 20) {
-      ageName = age + ' years old';
-    } else if (early.indexOf(parseInt(lastDigit)) > -1) {
-      ageName = 'early ' + (age - parseInt(lastDigit)) + "'s";
-    } else if (mid.indexOf(parseInt(lastDigit)) > -1) {
-      ageName = 'mid ' + (age - parseInt(lastDigit)) + "'s";
-    } else if (late.indexOf(parseInt(lastDigit)) > -1) {
-      ageName = 'late ' + (age - parseInt(lastDigit)) + "'s";
-    } else {
-      ageName = "It's a mystery";
-    }
-    return ageName;
-  };
 
   const getGenders = () => {
     return genderList
@@ -145,7 +122,7 @@ const ProfileTopPart = props => {
             <Text style={[styles.locationText]}>
               {location ? location : 'Narnia'}
             </Text>
-            {', ' + getAge() + ', '}
+            {', ' + getFormattedAge(birthyear)}
             {getGenders()}
           </Text>
         </View>
